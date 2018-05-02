fa0735d52da92b37e5dc06fd9352a7e17410ad8a
Jedreck <a302720001@outlook.com>
2018-05-02 21:57:42 +0800

    1

---
+++ SuperMarketManager/Controllers/DBManager/DBManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Web;
+
+namespace TestMysql.Controllers.DBManager
+{
+    public class DBManager
+    {
+        public static OdbcConnection GetOdbcConnection()
+        {
+            //自己的MySql连接方式，每个人不同，所以需要自己写
+            return new OdbcConnection("Dsn=MySql;uid=root;");
+        }
+    }
+}
