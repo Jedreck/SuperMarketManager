479e1126ede7e62cd534ba0160640e5df8f4808e
laucy <1170664318@qq.com>
2018-05-03 10:09:53 +0800

    更改namespace

--- SuperMarketManager/Controllers/Login/Employee.cs
+++ SuperMarketManager/Controllers/Login/Employee.cs
@@ -7,13 +7,13 @@ using System.Linq;
 using System.Web;
 using TestMysql.Models;
 
-namespace TestMysql.Controllers.Login
+namespace SuperMarketManager.Controllers.Login
 {
     public class Employee_C
     {
         public static Employee Login(string id, string pwd)
         {
-            OdbcConnection sqlConnection1 = DBManager.GetOdbcConnection();
+            OdbcConnection sqlConnection1 = DBManager.DBManager.GetOdbcConnection();
             sqlConnection1.Open();
             OdbcCommand odbcCommand = new OdbcCommand("select * from employee where E_ID='" + id + "' and E_Password='" + pwd + "'", sqlConnection1);
             OdbcDataReader odbcDataReader = odbcCommand.ExecuteReader();
