2a1731098621ec08ac89ee89fdbc5b3110d148e3
Jedreck <a302720001@outlook.com>
2018-05-03 10:27:33 +0800

    修改namespace的bug

--- SuperMarketManager/Views/Login/Login.aspx.cs
+++ SuperMarketManager/Views/Login/Login.aspx.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿using SuperMarketManager.Controllers.Login;
+using SuperMarketManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using TestMysql.Controllers.Login;
-using TestMysql.Models;
 
-namespace TestMysql.Views.Login
+namespace SuperMarketManager.Views.Login
 {
     public partial class Login : System.Web.UI.Page
     {
