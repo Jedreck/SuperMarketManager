585a2188919f3f659982f2bb3b18c09502236624
ooxian <1014584277@qq.com>
2018-05-06 16:53:32 +0800

    员工个人信息

---
+++ SuperMarketManager/Controllers/Person/Person_C.cs
@@ -0,0 +1,17 @@
+﻿using SuperMarketManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperMarketManager.Controllers.Person
+{
+    public class Person_C
+    {
+        //员工只具有修改个人信息的权利
+        public static bool alterById(Employee employee)
+        {
+            return Employee_C.AlterByID(employee);
+        }
+    }
+}
