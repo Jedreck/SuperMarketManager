7488cac9bd130083f3def21ae5c30859586a3730
ooxian <1014584277@qq.com>
2018-05-12 11:09:41 +0800

    入库

--- SuperMarketManager/Controllers/GoodsIn/GoodsIn_C.cs
+++ SuperMarketManager/Controllers/GoodsIn/GoodsIn_C.cs
@@ -13,10 +13,35 @@ namespace SuperMarketManager.Controllers
         //入库
         public static GoodsIn AddGoods(GoodsIn goodsin)
         {
+            bool flag;
             String insertSql = String.Format("insert into `marketmanage`.`goodsin`  (`GI_ID`, `G_ID`,`S_ID`,`GI_PriceIn`,`GI_Num`,`GI_Date`,`GI_OriginPrice`) " +
                 "values ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}')"
                 , goodsin.GI_ID, goodsin.G_ID, goodsin.S_ID, goodsin.PriceIn, goodsin.Num, goodsin.Date, goodsin.OriginPlace);
-            return ExecuteSQL.ExecuteNonQuerySQL_GetBool(insertSql) ? goodsin : null;            
+            flag=ExecuteSQL.ExecuteNonQuerySQL_GetBool(insertSql);
+            String sql = "update Goods set G_Store=G_Store+"+goodsin.Num+"where G_ID='"+goodsin.G_ID+"'";
+            ExecuteSQL.ExecuteNonQuerySQL_GetBool(sql);
+            return flag? goodsin : null;            
+        }
+
+        //全部入库信息
+        public static List<GoodsIn> GetGoodsIn_ListsByG_ID(string G_ID = "")
+        {
+            string sql = "select `googsin`.`GI_ID`,`goodsin`.`G_ID`,`goodsin`.`S_ID`,`goodsin`.`GI_PriceIn`,`goodsin`.`GI_Num`,`goodsin`.`GI_Date`,`goodsin`.`GI_OriginPrice` "
+                + "from `goodsin` "
+                + "where `goodsin`.`G_ID` =`" + G_ID + "`"
+                + "order by G_ID";
+            OdbcConnection odbcConnection = DBManager.GetOdbcConnection();
+            odbcConnection.Open();
+            OdbcCommand odbcCommand = new OdbcCommand(sql, odbcConnection);
+            OdbcDataReader odbcDataReader = odbcCommand.ExecuteReader();
+            if (odbcDataReader.HasRows)
+            {
+                List<GoodsIn> list = GoodsIn.getList(odbcDataReader);
+                odbcConnection.Close();
+                return list;
+            }
+            odbcConnection.Close();
+            return null;
         }
     }
 }
