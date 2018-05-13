8bcdecf231246ee39392706aa11de081efa10df4
Jedreck <a302720001@outlook.com>
2018-05-13 17:17:53 +0800

    格式化代码

--- SuperMarketManager/Views/PSInfo/PSInfo.aspx.cs
+++ SuperMarketManager/Views/PSInfo/PSInfo.aspx.cs
@@ -1,14 +1,9 @@
 ﻿using SuperMarketManager.Controllers;
 using SuperMarketManager.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Data.Odbc;
 namespace SuperMarketManager.Views.PSInfo
-  
+
 {
     public partial class PSInfo : System.Web.UI.Page
     {
@@ -20,27 +15,27 @@ namespace SuperMarketManager.Views.PSInfo
         {
             id.Text = Session["E_ID"].ToString();
             OdbcConnection odbcConnection = DBManager.GetOdbcConnection();
-           odbcConnection.Open();
+            odbcConnection.Open();
             //从员工表中提取和登录号一致的员工信息
-            OdbcCommand cmd = new OdbcCommand("select * from employee where E_ID='"+id.Text+"'", odbcConnection);
+            OdbcCommand cmd = new OdbcCommand("select * from employee where E_ID='" + id.Text + "'", odbcConnection);
             OdbcDataReader dr = cmd.ExecuteReader();
             while (dr.Read())
             {
 
-                  name.Text = dr["E_Name"].ToString();
+                name.Text = dr["E_Name"].ToString();
                 sex.Text = dr["E_Sex"].ToString();
                 phone.Text = dr["E_Phone"].ToString();
-               birth.Text = dr["E_Birth"].ToString();
+                birth.Text = dr["E_Birth"].ToString();
                 bankaccount.Text = dr["E_BankAccount"].ToString();
                 email.Text = dr["E_Email"].ToString();
-                position.Text =dr["E_Position"].ToString();
+                position.Text = dr["E_Position"].ToString();
 
             }
             dr.Close();
             dr.Close();
 
         }
-       
+
         protected void Back_Click(object sender, EventArgs e)
         {
             Response.Redirect("/Views/PSInfo/PSInfo.aspx");
@@ -69,5 +64,5 @@ namespace SuperMarketManager.Views.PSInfo
 
         }
     }
-        
+
 }
